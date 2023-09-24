f4f9aeb19a779f36f182d2013783b14f5e76b5f6
zferfaille <zferfaille@ensc.fr>
2023-09-24 23:06:48 +0200

    3e commit

--- script2.js
+++ script2.js
@@ -1,95 +1,147 @@
-window.addEventListener('load', function() {
-    setActiveTab('en_ce_moment'); // Définit l'onglet "en_ce_moment" comme actif par défaut
-    setVisibility('en_ce_moment_div'); 
-    setCVScroll(); // Gérer la barre de défilement pour le CV
-});
+// Les contenus des onglets en tant que chaînes de caractères
+var tabContents = {
+  'tab1': `
+  <h1 class="titre">En ce moment</h1>   
+    		
+  <img src="logo_stage.png" alt="logoISA" id="logo_isa"/>
 
 
-document.getElementById('en_ce_moment').addEventListener('click', function() {
-    setActiveTab('en_ce_moment');
-    setVisibility('en_ce_moment_div');
-});
+      <p id="p_stage">Actuellement en stage, j'occupe le poste de chargée de projet de recherche en comportement alimentaire.
+      Le projet est un Appel à Manifestation d'Intérêt de la Métropole Lilloise, en collaboration avec <a href="https://www.junia.com/fr/" target="_blank">JUNIA</a> et l'<a href="https://lillethics.com/anthropo-lab/" target="_blank">Anthropo-Lab</a>.
+      JUNIA développe une expertise et une offre autour de l'analyse sensorielle, et l’Anthropo-Lab est un laboratoire d'anthropologie expérimentale qui vise à comprendre et faire évoluer les comportements humains. Mon rôle est d’identifier des marqueurs physiologiques qui traduisent l’appréciation et les émotions lors d’un acte de consommation.
+      Mes missions sont : <br><br>
+      ● Recherches bibliographiques<br>
+      ● Mise en place d’expérimentations<br>
+      ● Analyses des résultats<br>
+      ● Présentation des résultats<br>
+      </p>
+`,
+'tab2': `
+<h1 class="titre">Réalisations</h1>
+</table>
+	
+<table class="table table-bordered">
+<tbody>
+  <tr>
+    <td class="card">
+      <img src="openBCI.jfif" alt="Image de la carte" class="card-img">
 
-document.getElementById('realisations').addEventListener('click', function() {
-    setActiveTab('realisations');
-    setVisibility('realisations_div');
- setRealisationsScroll();
+        <h5 class="card-title">Stage Ingénieure de Recherche - 2022</h5>
+        <p class="card-text">J'ai travaillé un mois aux côtés d'une Ingénieure de Recherche dans un laboratoire de Sciences cognitives Scalab à Lille. Mes missions étient de prendre en main un casque EEG OpenBCI, de faire une fiche technique et de réaliser un démonstrateur. J'ai pu mettre en place mes compétences en langage Python pour coder un démonstrateur capable de streamer les données du casque en direct, de diffuser sur l'écran une stimulation visuelle périodique et d'enregistrer et traiter les données sous formes de graphiques.</p>
+        <a href="https://docs.google.com/document/d/1Fok7RhiAk4yhXiB-a9YoCXeTK-VQ9idydssgww2hLZg/edit?usp=sharing" target="_blank" class="btn btn-primary">Voir le rapport</a>
 
-});
+    </td>
+  </tr>
+</tbody>
+</table>
+<table class="table table-bordered">
+<tbody>
+  <tr>
+    <td class="card">
+      <img src="TER.jfif" alt="Image de la carte" class="card-img">
 
-document.getElementById('competences').addEventListener('click', function() {
-    setActiveTab('competences');
-    setVisibility('competences_div');
-});
+        <h5 class="card-title">Travail Encadré de Recherche (TER) - 2020 / 2021</h5>
+        <p class="card-text">Projet encadré par un chercheur en sciences cognitives sur l'étude du lien entre la perception d'objets dans un environnement 3D et la motricité. J'ai eu l'occasion de faire une revue de littérature, d'organiser les passations expérimentales, d'analyser les données, et de rédiger le rapport sous forme d'article scientifique</p>
+        <a href="https://drive.google.com/file/d/17PeNToYXTp0lUhW8oK-twm2W5qe-eSw3/view?usp=sharing" target="_blank"class="btn btn-primary">Voir le rapport</a>
 
-document.getElementById('contact').addEventListener('click', function() {
-    setActiveTab('contact');
-    setVisibility('contact_div');
-});
+    </td>
+  </tr>
+</tbody>
+</table>
+<table class="table table-bordered">
+<tbody>
+  <tr>
+    <td class="card">
+      <img src="UXCleaner.png" alt="Image de la carte" class="card-img">
 
-document.getElementById('cv').addEventListener('click', function() {
-    setActiveTab('cv');
-    setVisibility('cv_div');
-     setCVScroll();
-});
+        <h5 class="card-title">UXCleaner - 2023</h5>
+        <p class="card-text">Réalisation d'une extension web permettant de détecter les dark pattern et le dark UX. Implémentation d'un score permettant d'évaluer la fiabilité d'un site web. Réalisation d'une maquette et observations d'utilisateurs. Mon rôle était à la fois d'encadrer une partie de l'équipe en mettant en application mes apprentissages de gestion de projet et de connaissances. Je faisais également partie du pôle UX, en charge de la maquette de l'extension, de l'idéation des détections de dark patterns, et de mise en place des oberservations utilisateurs.</p>
+        <a href="https://www.figma.com/proto/uiSQZU10A8I15pCKNXH1ky/Maquette_-UXCleaner_VRORO-(Copy)?node-id=104-323&starting-point-node-id=1%3A2&mode=design" target="_blank" class="btn btn-primary">Voir la maquette</a>
 
-//fonction qui permet de rendre les onglets actifs/non actifs pour le survol 
-function setActiveTab(tabId) {
-    var tabElements = document.querySelectorAll('.sidebar li a');
-    for (var i = 0; i < tabElements.length; i++) {
-        var tabElement = tabElements[i];
-        if (tabElement.id === tabId) {
-            tabElement.parentNode.classList.add('active');
-        } else {
-            tabElement.parentNode.classList.remove('active');
-        }
+    </td>
+  </tr>
+</tbody>
+</table>
+
+<table class="table table-bordered">
+<tbody>
+  <tr>
+    <td class="card">
+      <img src="logoETE.png" alt="Image de la carte" class="card-img">
+
+        <h5 class="card-title">Projet Entraîne Ton Estime (ETE) - 2021</h5>
+        <p class="card-text">Le projet visait la réalisation d'un serious game permettant l'entraînement et l'amélioration de l'estime de soi. Etant dans le pôle Recherche, mes missions étaient les suivantes : recherches sur l'état de l'art, idéation d'un protocole expérimental, création de tests permettant d'évaluer l'estime de soi et passations expérimentales. </p>
+        
+    </td>
+  </tr>
+</tbody>
+</table>		
+
+<table class="table table-bordered">
+<tbody>
+  <tr>
+    <td class="card">
+      <img src="logoMIXSC.png" alt="Image de la carte" class="card-img">
+
+        <h5 class="card-title">M.I.x.S.C - 2021-2022</h5>
+        <p class="card-text">Notre projet avec un client extérieur était d'étudier le lien entre les émotions et la musique. Nous avons donc créé une musique utilisant les battements bi-neuronaux, permettant la relaxation. Pour mesurer le niveau de relaxation; nous avons utilisé le casque EEG EMOTIV. Nous avons ainsi réalisé un état de l'art, crée une musique ainsi qu'un protocole expérimental. Enfin nous avons traité et analysé les données. </p>
+        
+    </td>
+  </tr>
+</tbody>
+</table>	
+`,
+'tab3': `
+  <h1 class="titre">Compétences</h1>
+  <table>
+      <thead>
+          <tr>
+              <th>Langages informatiques</th>
+              <th>Facteurs humains</th>
+              <th>Soft Skills</th>
+          </tr>
+      </thead>
+      <tbody>
+          <tr>
+              <td>Python<br> C#<br> HTML<br> CSS<br> JavaScript<br> PHP<br> SQL<br> RStudio <br> Kotlin <br> Matlab </td>
+              <td>CCU<br> UXDesign<br> Maquettage<br> Conception d'IHM</td>
+              <td>Curiosité<br> Adaptabilité<br> Empathie</td>
+          </tr>
+      </tbody>
+  </table>
+`,
+'tab4': `
+  <h1 class="titre">Contact</h1>
+  <strong>Adresse mail :</strong>
+  <span class="email">zferfaille@ensc.fr</span>
+  <p>
+      <strong>Linkedin :</strong>
+      <a href="https://www.linkedin.com/in/zo%C3%A9-ferfaille/" target="_blank" id="in">
+          <img src="inn.png" alt="profil" id="logo_in"/>
+      </a>
+  </p>
+`,
+'tab5': `
+<img src="cv_portfolio.png" alt="profil"  width="800" height="1200" />
+`,
+  };
+  
+  function changeTab(tabId) {
+    // Récupérer la div de contenu
+    var contentDiv = document.getElementById('content');
+  
+    // Effacer le contenu actuel
+    contentDiv.innerHTML = '';
+  
+    // Injecter le contenu approprié en fonction de l'onglet cliqué
+    if (tabContents.hasOwnProperty(tabId)) {
+      contentDiv.innerHTML = tabContents[tabId];
+    } else {
+      contentDiv.innerHTML = 'Contenu par défaut';
     }
-}
+  }
+  
 
-
-
-
-
-function setVisibility(tabId) {
-    var tabElements = document.querySelectorAll('.selected-items-box div');
-    for (var i = 0; i < tabElements.length; i++) {
-        var tabElement = tabElements[i];
-
-        if (tabElement.id === tabId) {
-            tabElement.style.visibility = "visible";
-            showChildrenElements(tabElement);  
-        } else {
-            tabElement.style.visibility = "hidden";
-            hideChildrenElements(tabElement);
-        }
-    }
-}
-
-function showChildrenElements(element) {
-    var children = element.querySelectorAll('*');
-    for (var i = 0; i < children.length; i++) {
-        children[i].style.visibility = "visible";
-    }
-}
-
-function hideChildrenElements(element) {
-    var children = element.querySelectorAll('*');
-    for (var i = 0; i < children.length; i++) {
-        children[i].style.visibility = "hidden";
-    }
-}
-
-
-
-function setCVScroll() {
-    var cvDiv = document.getElementById('cv_div');
-    cvDiv.style.overflowY = "auto"; // Ajoute une barre de défilement verticale
-    cvDiv.style.height = "400px"; 
-}
-
-function setRealisationsScroll() {
-    var realDiv = document.getElementById('realisations_div');
-   realDiv.style.overflowY = "auto"; // Ajoute une barre de défilement verticale
-    realDiv.style.height = "400px"; 
-}
-
+  window.onload = function() {
+    changeTab('tab1');
+  };
